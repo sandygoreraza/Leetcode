2f815685a3371d2ebdeaff486313e70c1ee58676
Greenmile <Greenmile@DESKTOP-OLJAAKA>
2024-11-30 11:51:21 +0200

    Add project files.

---
+++ Leetcode/AddingArray.cs
@@ -0,0 +1,39 @@
+﻿namespace Leetcode
+{
+    public class AddingArray
+    {
+
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+             
+
+                    for (int j = i+1; j < nums.Length ; j++)
+                    {
+                        
+                        sum = nums[i] + nums[j];
+                 
+                        if (sum == target)
+                        {
+
+                            int[] newindices = { i, j };
+
+                            return newindices;
+                        }
+
+                    }
+
+               
+
+            }
+
+            return new int[0];
+        }
+
+    }
+}
